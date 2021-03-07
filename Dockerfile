c95b09bf41c359361317f30e8d1d330b31e36a9c
mvadu <8618235+mvadu@users.noreply.github.com>
2021-03-07 01:23:55 -0500

    add tests, multi stage build for docker

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,26 @@
-FROM python:3
+FROM python:3 as base
 
 RUN apt-get install libbluetooth-dev
-
 RUN pip install bluepy
 RUN pip install pybluez
 RUN pip install requests
 RUN pip install dynaconf
-RUN pip install requests
 RUN git clone https://github.com/colin-guyon/py-bluetooth-utils.git
 RUN cp py-bluetooth-utils/bluetooth_utils.py .
-#confuse expects yaml extn
+#copy the default config file
 COPY config_default.yml config_default.yaml 
 COPY mi_atc_reader.py mi_atc_reader.py
 
+from base
+RUN pip install pytest
+RUN python -m pytest mi_atc_reader.py -rA
+
+from base
+LABEL "contact"="info@adystech.com"
+LABEL repo="https://github.com/AdysTech/mi_atc_reader/"
+LABEL version="1.0"
+LABEL description="Provides a python script as a docker service  \
+which can read BLE advertisements from Xiaomi Smart Bluetooth Thermometer & Hygrometer. \
+Thermometers needs to be running custom open source firmware from pvvx or atc1441"
 ENTRYPOINT python ./mi_atc_reader.py
 #ENTRYPOINT /bin/bash
--- mi_atc_reader.py
+++ mi_atc_reader.py
@@ -25,6 +25,7 @@ class SensorReading:
     battery: int = 0
     timestamp: int = 0
     sensor: dict = None
+    counter: int = 0
 
     def from_dict(self, d):
         if not isinstance(d, dict):
@@ -40,130 +41,196 @@ class SensorReading:
             return d  # Not a dataclass field
 
 
-config = Dynaconf(
-    settings_files=['config_default.yaml'],
-    envvar_prefix="ATC",
-)
+def le_advertise_packet_handler(mac, adv_type, data, rssi):
+    # Check to make sure we are getting this message from one of ATC firmware forks.
+    # both atc1441 and pvvx forks send data on GATT Service 0x181A Environmental Sensing
+    if struct.unpack('<H', data[3:5])[0] != 0x181A:
+        return
 
-try:
-    custom_config_file = 'custom.yml'
-    # Add config items from specific file
-    if os.path.exists(custom_config_file):
-        config.load_file(custom_config_file)
-except:
-    sys.stderr.write("Couldn't load config files")
-    raise
+    # data format adds mac as part of data structure. just another check to ensure we are not listening to any other BLE on 0x181A
+    data_mac, pvvx = get_data_mac(data)
+    if(mac.upper() != data_mac.hex(':').upper()):
+        return
 
-numeric_level = getattr(logging, config.logging.level.upper(), None)
-if not isinstance(numeric_level, int):
-    raise ValueError(f'Invalid log level: {config.logging.level}')
-logging.basicConfig(
-    format='%(asctime)-15s %(levelname)s: %(message)s', level=numeric_level)
-if numeric_level > logging.INFO and config.discovery_mode:
-    print("Device discovery messages may not appear in the log in current log level. Needs to be at least at INFO")
+    reading = parse_pvvx_format(data) if pvvx else parse_atc_format(data)
+    if reading == None:
+        return
 
+    if config.discovery_mode:
+        logging.info(
+            f"Device: {mac} Temp: {reading.temperature}c Humidity: {reading.humidity}% Batt: {reading.battery}% ({reading.voltage}v)")
 
-print('using configuration')
-print(config.as_dict())
+    if len(config.thermometers) > 0:
+        if any((match := t)['mac'] == mac for t in config.thermometers):
+            reading.sensor = match
+            readingQueue.append(reading)
 
-dev_id = config.ble.device_id
-logging.info(f'Enabling bluetooth device {dev_id}')
-toggle_device(dev_id, True)
-
-try:
-    sock = bluez.hci_open_dev(dev_id)
-except:
-    logging.error(f"Cannot open bluetooth device {dev_id}")
-    raise
-
+def handle_retry(reading:SensorReading):
+    if len(readingQueue) < config.errorbuffer.max_items:
+        readingQueue.appendleft(reading)
+    else:
+        logging.warning(f'retry queue full, Discoreded Message: {reading}!')
+    exit_event.wait(5)
 
 def deque_thread():
     while True:
         try:
+            logging.debug(f"Reading Queue Depth: {len(readingQueue)}")
             reading = readingQueue.popleft()
+            logging.debug(reading)
             if config.influxdb.enabled:
                 tags = ','.join([f"{key}={value}" for key,
                                  value in reading.sensor['tags'].items()])
-                data = f"{config.influxdb.measurement},{tags},name={reading.sensor['name']},mac={reading.sensor['mac']} temperature={reading.temperature},humidity={reading.humidity},battery={reading.battery},voltage={reading.voltage} {reading.timestamp}"
-                logging.debug(f'url:{influxdb_write_endpoint}, data: {data}')
-                r = requests.post(influxdb_write_endpoint, data=data)
-                if r.status_code != 204:
-                    logging.warning(f'Failed to save{reading} due to {r.text}')
+                payload = f"{config.influxdb.measurement},{tags},name={reading.sensor['name']},mac={reading.sensor['mac']} temperature={reading.temperature},humidity={reading.humidity},battery={reading.battery},voltage={reading.voltage} {reading.timestamp}"
+                try:
+                    r = requests.post(influxdb_write_endpoint,
+                                    data=payload, timeout=1)
+                    # ', return:{r.status_code}')
+                    logging.debug(f'url:{influxdb_write_endpoint}, data: {payload}')
+                    if r.status_code != 204:                        
+                        if r.status_code == 404:
+                            logging.warning(f'Influxdb database missing!!.. manually create the DB.')
+                            handle_retry(reading)
+                        else:
+                            logging.warning(f'Failed to save{reading} due to {r.text}')
+                except requests.Timeout:
+                    logging.warning(f'Influxdb Timeout.. retrying')
+                    handle_retry(reading)
+                except requests.ConnectionError:
+                    logging.warning(f'Unable to connect to InfluxDB.. retrying')
+                    handle_retry(reading)
         except IndexError:
             exit_event.wait(1)
         except Exception as e:
-            logging.exception(e)
+            logging.exception(e)            
         if exit_event.is_set():
-            if leftout := len(readingQueue) > 0:
-                logging.warning(f"Queue still has {leftout} items")
+            if len(readingQueue) > 0:
+                logging.warning(f"Queue had {len(readingQueue)} unsaved items")
             break
 
 
-exit_event = threading.Event()
-readingQueue = deque()
-queueThread = threading.Thread(target=deque_thread)
-queueThread.start()
+# ref: https://github.com/pvvx/ATC_MiThermometer#bluetooth-advertising-formats
+# this code will support both ATC format and pvvx's custom format.
+# https://github.com/pvvx/ATC_MiThermometer/blob/c3b89a7eddad21c054e352b64af21654d5112421/src/ble.h#L90
+
+def get_data_mac(data):
+    pvvx = False
+    data_mac = bytearray(data[5:11])
+    # pvvx format sends everything in little endian format. Python assumes big endian.
+    if len(data) > 18 and len(data) < 22:
+        data_mac.reverse()
+        pvvx = True
+    return data_mac, pvvx
 
 
-if config.influxdb.enabled:
-    influxdb_write_endpoint = f"{config.influxdb.url}/write?db={config.influxdb.database}&precision={config.influxdb.precision}"
-    logging.debug(f'writing to {influxdb_write_endpoint}')
-# Set filter to "True" to see only one packet per device
-logging.info('Start scanning for BLE messages')
-enable_le_scan(sock, filter_duplicates=False)
-
-try:
-    def le_advertise_packet_handler(mac, adv_type, data, rssi):
-
-        # ref: https://github.com/pvvx/ATC_MiThermometer#bluetooth-advertising-formats
-        # this code will support both ATC format and pvvx's custom format.
-        # https://github.com/pvvx/ATC_MiThermometer/blob/c3b89a7eddad21c054e352b64af21654d5112421/src/ble.h#L90
-
-        pvvx = False
-        data_mac = bytearray(data[5:11])
-        # pvvx format sends everything in little endian format. Python assumes big endian.
-        if len(data) > 18 and len(data) < 22:
-            data_mac.reverse()
-            pvvx = True
-
-        # data format adds mac as part of data structure. just another check to ensure we are not listening to any other BLE on 0x181A
-        if(mac.upper() != data_mac.hex(':').upper()):
-            return
-
-        # Check to make sure we are getting this message from one of ATC firmware forks.
-        # both atc1441 and pvvx forks send data on GATT Service 0x181A Environmental Sensing
-        if struct.unpack('<H', data[3:5])[0] != 0x181A:
-            return
-
+def parse_atc_format(data) -> SensorReading:
+    try:
         reading = SensorReading(0, 0, 0, 0, int(time.time()))
-        if pvvx:
-            reading.temperature, reading.humidity, reading.voltage, reading.battery = struct.unpack(
-                '<hHHB', data[11:18])
-            reading.temperature /= 100.0
-            reading.humidity /= 100.0
-            reading.voltage /= 1000
-        else:
-            reading.temperature, reading.humidity, reading.battery, reading.voltage = struct.unpack(
-                '>hBBh', data[11:17])
-            reading.temperature /= 10.0
-            reading.voltage /= 1000
+        reading.temperature, reading.humidity, reading.battery, reading.voltage, reading.counter = struct.unpack(
+            '>hBBhB', data[11:18])
+        reading.temperature /= 10.0
+        reading.voltage /= 1000
+        return reading
+    except Exception as e:
+        logging.exception(e)
+        return None
 
-        if config.discovery_mode:
-            logging.info(
-                f"Device: {mac} Temp: {reading.temperature}c Humidity: {reading.humidity}% Batt: {reading.battery}% ({reading.voltage}v)")
-        if len(config.thermometers) > 0:
-            if any((match := t)['mac'] == mac for t in config.thermometers):
-                reading.sensor = match
-                readingQueue.append(reading)
 
-    # Blocking call (the given handler will be called each time a new LE
-    # advertisement packet is detected)
-    parse_le_advertising_events(sock,
-                                handler=le_advertise_packet_handler,
-                                debug=False)
-# Scan until Ctrl-C
-except KeyboardInterrupt:
-    logging.info('received exit signal')
-    disable_le_scan(sock)
-    logging.info('waiting for background operations to complete')
-    exit_event.set()
+def parse_pvvx_format(data) -> SensorReading:
+    try:
+        reading = SensorReading(0, 0, 0, 0, int(time.time()))
+        reading.temperature, reading.humidity, reading.voltage, reading.battery, reading.counter = struct.unpack(
+            '<hHHBB', data[11:19])
+        reading.temperature /= 100.0
+        reading.humidity /= 100.0
+        reading.voltage /= 1000
+        return reading
+    except Exception as e:
+        logging.exception(e)
+        return None
+
+
+def loadConfig() -> Dynaconf:
+    config = Dynaconf(
+        settings_files=['config_default.yaml'],
+        envvar_prefix="ATC",
+    )
+    try:
+        custom_config_file = 'custom.yml'
+        # Add config items from specific file
+        if os.path.exists(custom_config_file):
+            config.load_file(custom_config_file)
+    except:
+        sys.stderr.write("Couldn't load config files")
+        raise
+    return config
+
+
+if __name__ == '__main__':  # has a blocking call at the end
+    config = loadConfig()
+    log_level = getattr(logging, config.logging.level.upper(), None)
+    if not isinstance(log_level, int):
+        raise ValueError(f'Invalid log level: {config.logging.level}')
+    logging.basicConfig(level=log_level,
+                        format='%(asctime)-15s %(levelname)s: %(message)s')
+    print('using configuration')
+    print(config.as_dict())
+    print(
+        f'Current log level: {logging.getLevelName(logging.getLogger().getEffectiveLevel())}')
+
+    if log_level > logging.INFO and config.discovery_mode:
+        print("Device discovery messages may not appear in the log in current log level. Needs to be at least at INFO")
+
+    dev_id = config.ble.device_id
+    logging.info(f'Enabling bluetooth device {dev_id}')
+    toggle_device(dev_id, True)
+
+    try:
+        sock = bluez.hci_open_dev(dev_id)
+    except:
+        logging.error(f"Cannot open bluetooth device {dev_id}")
+        raise
+
+    if config.influxdb.enabled:
+        influxdb_write_endpoint = f"{config.influxdb.url}/write?db={config.influxdb.database}&precision={config.influxdb.precision}"
+        logging.debug(f'writing to {influxdb_write_endpoint}')
+    # Set filter to "True" to see only one packet per device
+    logging.info('Start scanning for BLE messages')
+    enable_le_scan(sock, filter_duplicates=False)
+
+    exit_event = threading.Event()
+    readingQueue = deque()
+    queueThread = threading.Thread(target=deque_thread)
+    #queueThread.daemon = True
+    queueThread.start()
+
+    try:
+        # Blocking call (the given handler will be called each time a new LE
+        # advertisement packet is detected)
+        parse_le_advertising_events(sock,
+                                    handler=le_advertise_packet_handler,
+                                    debug=config.ble.debug)
+    # Scan until Ctrl-C
+    except KeyboardInterrupt:
+        logging.info('received exit signal')
+        disable_le_scan(sock)
+        logging.info('waiting for background operations to complete')
+        exit_event.set()
+
+
+class TestClassAtcReader:
+    def test_pvvx_parser(self):
+        data = bytearray.fromhex('1312161a18332211ccbbaa670981108f0b54af04')
+        r = parse_pvvx_format(data)
+        assert r != None
+        assert r.temperature == 24.07
+        assert r.humidity == 42.25
+        assert r.voltage == 2.959
+
+    def test_atc_parser(self):
+        data = bytearray.fromhex('1110161a18332211ccbbaa00f02a540b8faf')
+        r = parse_atc_format(data)
+        assert r != None
+        assert r.temperature == 24.0
+        assert r.humidity == 42
+        assert r.voltage == 2.959
